0f071614b11dda75454a5e531059ac6f295bbff5
ugur cakil <ugur.c@freelancer.run>
2025-07-23 21:19:26 +0100

    base

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-input',
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent {}
